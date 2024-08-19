708a65ff85658362907b0b455a3731d5c8053cb5
Athul eldho <23mp2087@rit.ac.in>
2024-08-19 23:02:01 +0530

    registration codes completed.

--- src/coding.py
+++ src/coding.py
@@ -1,5 +1,7 @@
 from flask import *
 from src.dbconnectionnew import *
+import os
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
@@ -36,9 +38,66 @@ def user_registration():
     return render_template("useReg.html")
 
 
+@app.route("/userregister_code", methods=['post'])
+def userregister_code():
+    fname = request.form["textfield"]
+    lname = request.form["textfield2"]
+    email = request.form["textfield3"]
+    phno = request.form["textfield4"]
+    address = request.form["textfield5"]
+    username = request.form["textfield6"]
+    password = request.form["textfield7"]
+
+    qry = "INSERT INTO `login` VALUES(NULL, %s, %s, 'user')"
+    val = (username, password)
+    id = iud(qry, val)
+
+    qry = "INSERT INTO `user` VALUES(NULL, %s, %s, %s, %s, %s, %s)"
+    val = (id, fname, lname, email, phno, address)
+
+    iud(qry, val)
+
+    return '''<script>alert("Successfully registered");window.location="/"</script>'''
+
+
 @app.route("/provider_registration")
 def provider_registration():
     return render_template("providerReg.html")
+
+
+@app.route("/proregister_code", methods=['post'])
+def proregister_code():
+    fname = request.form["textfield"]
+    lname = request.form["textfield2"]
+    email = request.form["textfield3"]
+    phno = request.form["textfield4"]
+    place = request.form["textfield5"]
+    pin = request.form["textfield6"]
+    service = request.form["textfield7"]
+    doc = request.files["file2"]
+    noc = request.files["file"]
+    username = request.form["textfield8"]
+    password = request.form["textfield9"]
+
+    doc_name = secure_filename(doc.filename)
+    doc.save(os.path.join("static/uploads", doc_name))
+
+    noc_name = secure_filename(noc.filename)
+    noc.save(os.path.join("static/uploads", noc_name))
+
+
+    qry = "INSERT INTO `login` VALUES(NULL, %s, %s, 'pending')"
+    val = (username, password)
+    id = iud(qry, val)
+
+    qry = "INSERT INTO `serviceprovider` VALUES(NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (id, fname, lname, email, phno, place, pin, service, doc_name, noc_name)
+
+    iud(qry, val)
+
+    return '''<script>alert("Successfully registered");window.location="/"</script>'''
+
+
 
 
 @app.route("/block_provider")
@@ -80,7 +139,7 @@ def provider_home():
 
 @app.route("/user_home")
 def user_home():
-    return render_template("userhome.html")
+    return render_template("user/userhome.html")
 
 @app.route("/request_provder")
 def request_provder():
